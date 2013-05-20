b77eef1a5b96008001243f512ccfea6beae15081
Piotr B <pio4426@gmail.com>
2013-05-20 20:56:43 +0200

    Initial Commit

---
+++ WPLauncher/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WPLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        } 
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            pictureBox1.BackgroundImage = global::WPLauncher.Properties.Resources.logo6;
+            pictureBox1.Location = new System.Drawing.Point(305, 1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(335, 108);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.WaitOnLoad = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WPLauncher.Properties.Resources.wybierzkropkaminecraft;
+            this.button1.Location = new System.Drawing.Point(485, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 73);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 429);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(467, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "%appdata%\\.minecraft\\";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.button2.Image = global::WPLauncher.Properties.Resources.Instaluj1;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(646, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 73);
+            this.button2.TabIndex = 2;
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(897, 373);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://warpack.pl/launcher/news/index.php?q=main", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::WPLauncher.Properties.Resources.bg_main;
+            this.ClientSize = new System.Drawing.Size(921, 476);
+            this.Controls.Add(pictureBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Warpack Launcher";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WPLauncher/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+
+namespace WPLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text + "/bin/minecraft.jar"))
+            {
+                
+                using (ZipFile zFile = new ZipFile(textBox1.Text + "/bin/minecraft.jar"))
+                {
+                    zFile.BeginUpdate();
+                    try
+                    {
+                        zFile.Delete("META-INF/MANIFEST.MF");
+                    }
+                    catch
+                    { };
+                    try
+                    {
+                        zFile.Delete("META-INF/MOJANG_C.SF");
+                    }
+                    catch
+                    { };
+                    try
+                    {
+                        zFile.Delete("META-INF/MOJANG_C.DSA");
+                    }
+                    catch
+                    { };
+
+                    FileInfo[] files = null;
+                    DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory() + @"\do minecraft.jar\");
+                    string searchPattern = "*.*";
+                    files = di.GetFiles(searchPattern, SearchOption.AllDirectories);
+                    foreach (FileInfo f in files)
+                    {
+                        zFile.Add(f.FullName, f.FullName.Replace(Directory.GetCurrentDirectory() + @"\do minecraft.jar\", ""));
+                    }
+                    zFile.CommitUpdate();
+                    zFile.Close();
+
+
+                    //Now Create all of the directories
+                    foreach (string dirPath in Directory.GetDirectories(Directory.GetCurrentDirectory() + @"\do .minecraft\", "*", SearchOption.AllDirectories))
+                        Directory.CreateDirectory(dirPath.Replace(Directory.GetCurrentDirectory() + @"\do .minecraft\", textBox1.Text + @"\" ));
+
+                    
+                    di = new DirectoryInfo(Directory.GetCurrentDirectory() + @"\do .minecraft\");
+                    files = di.GetFiles(searchPattern, SearchOption.AllDirectories);
+                    
+                    foreach (FileInfo f in files)
+                    {
+                        
+                        File.Copy(f.FullName, textBox1.Text + f.FullName.Replace(Directory.GetCurrentDirectory() + @"\do .minecraft", ""), true);
+                    }
+                }
+                MessageBox.Show("Zrobione!");
+            }
+            else 
+            {
+                MessageBox.Show("Błąd! Niepoprawna ścieżka do pliku lub folder nie zawiera minecraft.jar");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
